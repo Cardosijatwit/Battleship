d67cfead48b2c0c68e708017c8248a6cc5f1d9a9
Jonny Cardosi <cardosij@wit.edu>
2022-04-05 12:27:03 -0400

    cool

--- Battleship/src/application/Main.java
+++ Battleship/src/application/Main.java
@@ -10,11 +10,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			BorderPane lane = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
