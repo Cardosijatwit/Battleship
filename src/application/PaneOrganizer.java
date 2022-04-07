7aabcab350c99a1bd1d907f0cb126c56ec86d58e
cardosij <cardosij@PF2XVEVD.wit.private>
2022-04-06 20:16:11 -0400

    Modified gameboard

--- src/application/PaneOrganizer.java
+++ src/application/PaneOrganizer.java
@@ -8,6 +8,8 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 public class PaneOrganizer {
 	
@@ -24,7 +26,20 @@ public class PaneOrganizer {
 	private void createGrid() {
 		
 		GridPane playerGrid = new GridPane();
-		for (int i = 0; i < 27; i++) {
+		for (int i = 0; i < 11; i++) {
+			playerGrid.getRowConstraints().add(new RowConstraints(50));
+			playerGrid.getColumnConstraints().add(new ColumnConstraints(50));
+		}
+		
+		playerGrid.getRowConstraints().add(new RowConstraints(550));
+		playerGrid.getColumnConstraints().add(new ColumnConstraints(250));
+		Text midt = new Text();
+		midt.setTextAlignment(TextAlignment.JUSTIFY);
+		midt.setText("Hello There Person");
+		GridPane.setConstraints(midt, 11, 11);
+		playerGrid.getChildren().add(midt);
+		
+		for (int i = 12; i < 23; i++) {
 			playerGrid.getRowConstraints().add(new RowConstraints(50));
 			playerGrid.getColumnConstraints().add(new ColumnConstraints(50));
 		}
@@ -39,7 +54,7 @@ public class PaneOrganizer {
 					button.setStyle("-fx-font-size: 1.72em; ");
 				}
 				else if (i >= 1 && i < 11 && j == 0) {
-					button.setText(Integer.toString(i));
+					button.setText(firstColumnCheck(i));
 					button.setStyle("-fx-font-size: 1.72em; ");
 				}
 				else if (i == 0 && j == 0){
@@ -64,19 +79,19 @@ public class PaneOrganizer {
 		}
 		
 		for (int i = 0; i < 11; i++) {
-			for(int j = 16; j <27; j++) {
+			for(int j = 12; j < 23; j++) {
 				Button button = new Button();
 				button.setPrefHeight(50);
 				button.setPrefWidth(50);
-				if (j >= 17 && j < 27 && i == 0) {
+				if (j >= 13 && j < 23 && i == 0) {
 					button.setText(firstRowCheck(j));
 					button.setStyle("-fx-font-size: 1.72em; ");
 				}
-				else if (i >= 1 && i < 11 && j == 16) {
-					button.setText(Integer.toString(i));
+				else if (i >= 1 && i < 11 && j == 12) {
+					button.setText(firstColumnCheck(i));
 					button.setStyle("-fx-font-size: 1.72em; ");
 				}
-				else if (i == 0 && j == 16){
+				else if (i == 0 && j == 12){
 					button.setText("+");
 					button.setStyle("-fx-font-size: 1.25em; ");
 				}
@@ -85,7 +100,7 @@ public class PaneOrganizer {
 				button.setOnAction(new EventHandler<ActionEvent>() {
 					@Override
 					public void handle(ActionEvent arg0) {
-						if ((GridPane.getRowIndex(button) >= 0 && GridPane.getRowIndex(button) < 11 && GridPane.getColumnIndex(button) == 16) || (GridPane.getColumnIndex(button) >= 16 && GridPane.getColumnIndex(button) < 27 && GridPane.getRowIndex(button) == 0)) {
+						if ((GridPane.getRowIndex(button) >= 0 && GridPane.getRowIndex(button) < 11 && GridPane.getColumnIndex(button) == 12) || (GridPane.getColumnIndex(button) >= 12 && GridPane.getColumnIndex(button) < 23 && GridPane.getRowIndex(button) == 0)) {
 						}
 						else {
 							System.out.println("Column " + (firstRowCheck(GridPane.getColumnIndex(button))));
@@ -105,36 +120,69 @@ public class PaneOrganizer {
 		return main;
 	}
 	
-	private String firstRowCheck(int c) {
-		if (c == 1 || c == 17) {
+	private String firstColumnCheck (int c) {
+		if (c == 1 || c == 13) {
 			return "A";
 		}
-		else if (c == 2 || c == 18) {
+		else if (c == 2 || c == 14) {
 			return "B";
 		}
-		else if (c == 3 || c == 19) {
+		else if (c == 3 || c == 15) {
 			return "C";
 		}
-		else if (c == 4 || c == 20) {
+		else if (c == 4 || c == 16) {
 			return "D";
 		}
-		else if (c == 5 || c == 21) {
+		else if (c == 5 || c == 17) {
 			return "E";
 		}
-		else if (c == 6 || c == 22) {
+		else if (c == 6 || c == 18) {
 			return "F";
 		}
-		else if (c == 7 || c == 23) {
+		else if (c == 7 || c == 19) {
 			return "G";
 		}
-		else if (c == 8 || c == 24) {
+		else if (c == 8 || c == 20) {
 			return "H";
 		}
-		else if (c == 9 || c == 25) {
+		else if (c == 9 || c == 21) {
 			return "I";
 		}
 		else {
 			return "J";
+		}
+	}
+	
+	private String firstRowCheck (int r) {
+		if (r == 1 || r == 13) {
+			return "1";
+		}
+		else if (r == 2 || r == 14) {
+			return "2";
+		}
+		else if (r == 3 || r == 15) {
+			return "3";
+		}
+		else if (r == 4 || r == 16) {
+			return "4";
+		}
+		else if (r == 5 || r == 17) {
+			return "5";
+		}
+		else if (r == 6 || r == 18) {
+			return "6";
+		}
+		else if (r == 7 || r == 19) {
+			return "7";
+		}
+		else if (r == 8 || r == 20) {
+			return "8";
+		}
+		else if (r == 9 || r == 21) {
+			return "9";
+		}
+		else {
+			return "10";
 		}
 	}
 	
